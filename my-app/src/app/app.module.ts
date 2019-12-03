75bf12358e1a72b3fdc3ec8931e86f55d59dc6d7
Alberto Paulo <Albertoenriquepaulo@yahoo.es>
2019-12-03 12:34:28 +0100

    Adding Routing to contact componen

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './encabezado/encabezado.component';
+import { FooterComponent } from './footer/footer.component';
+import { CuerpoComponent } from './cuerpo/cuerpo.component';
+import { CuerpodosComponent } from './cuerpodos/cuerpodos.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  { path: 'contacto', component: ContactoComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    FooterComponent,
+    CuerpoComponent,
+    CuerpodosComponent,
+    ContactoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
